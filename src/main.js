e68a262ccdb564aaa7bcd230e616ac0cadfaf5a0
pengpeng387 <l>
2019-10-25 21:14:05 +0800

    引入vue-router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入element-ui框架
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入router
+import router from '@/router'
 
 Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+// 引入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+  ]
+})
