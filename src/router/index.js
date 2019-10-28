381c80204550ba63bc79eba74420f02ff42818fd
pengpeng387 <l>
2019-10-28 09:32:52 +0800

    首页功能及显示完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,31 @@ import Vue from 'vue'
 // 引入vue-router
 import VueRouter from 'vue-router'
 import login from '@/views/login'
+import home from '@/views/home'
+import welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-  routes: [{
-    path: '/login',
-    component: login
-  }
+  routes: [
+    {
+      path: '/login',
+      component: login
+    },
+    {
+      path: '/',
+      component: home,
+      children: [
+        {
+          path: '/',
+          component: welcome
+        }
+      ]
+    },
+    {
+      path: '*',
+      component: NotFound
+    }
   ]
 })
